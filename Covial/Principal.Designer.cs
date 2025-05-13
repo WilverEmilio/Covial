290b962638120fb629264de678e0c2a53f01ea8e
Wilver Emilio Xiá Ixcot <125522983+WilverEmilio@users.noreply.github.com>
2025-05-12 22:26:24 -0600

    Agergando modelo para planes anual y asi mismo agregar el menu correspondiente para una mejor visualización!

--- Covial/Principal.Designer.cs
+++ Covial/Principal.Designer.cs
@@ -28,12 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Principal";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            menuStrip1 = new MenuStrip();
+            covialToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            cerrarSesiónToolStripMenuItem = new ToolStripMenuItem();
+            mantenimientoToolStripMenuItem = new ToolStripMenuItem();
+            usuariosToolStripMenuItem = new ToolStripMenuItem();
+            contratistasToolStripMenuItem = new ToolStripMenuItem();
+            tiposDeProgramaToolStripMenuItem = new ToolStripMenuItem();
+            planesAnualesToolStripMenuItem = new ToolStripMenuItem();
+            gestiónDeProgramasYProyectosToolStripMenuItem = new ToolStripMenuItem();
+            programasToolStripMenuItem = new ToolStripMenuItem();
+            proyectosToolStripMenuItem = new ToolStripMenuItem();
+            avancesDelProyectoToolStripMenuItem = new ToolStripMenuItem();
+            gestiónFinancieraToolStripMenuItem = new ToolStripMenuItem();
+            órdenesDePagoToolStripMenuItem = new ToolStripMenuItem();
+            consultasReportesToolStripMenuItem = new ToolStripMenuItem();
+            reportesDeProyectosToolStripMenuItem = new ToolStripMenuItem();
+            avancesDeProyectosToolStripMenuItem = new ToolStripMenuItem();
+            resumenDeÓrdenesDePagoToolStripMenuItem = new ToolStripMenuItem();
+            consultasPorContratistasFechasEstadoToolStripMenuItem = new ToolStripMenuItem();
+            verToolStripMenuItem = new ToolStripMenuItem();
+            ventanasToolStripMenuItem = new ToolStripMenuItem();
+            ayudaToolStripMenuItem = new ToolStripMenuItem();
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            menuStrip1.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { covialToolStripMenuItem, mantenimientoToolStripMenuItem, gestiónDeProgramasYProyectosToolStripMenuItem, gestiónFinancieraToolStripMenuItem, consultasReportesToolStripMenuItem, verToolStripMenuItem, ventanasToolStripMenuItem, ayudaToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1035, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // covialToolStripMenuItem
+            // 
+            covialToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { salirToolStripMenuItem, cerrarSesiónToolStripMenuItem });
+            covialToolStripMenuItem.Name = "covialToolStripMenuItem";
+            covialToolStripMenuItem.Size = new Size(52, 20);
+            covialToolStripMenuItem.Text = "Covial";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(180, 22);
+            salirToolStripMenuItem.Text = "Salir";
+            // 
+            // cerrarSesiónToolStripMenuItem
+            // 
+            cerrarSesiónToolStripMenuItem.Name = "cerrarSesiónToolStripMenuItem";
+            cerrarSesiónToolStripMenuItem.Size = new Size(180, 22);
+            cerrarSesiónToolStripMenuItem.Text = "Cerrar Sesión";
+            // 
+            // mantenimientoToolStripMenuItem
+            // 
+            mantenimientoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { usuariosToolStripMenuItem, contratistasToolStripMenuItem, tiposDeProgramaToolStripMenuItem, planesAnualesToolStripMenuItem });
+            mantenimientoToolStripMenuItem.Name = "mantenimientoToolStripMenuItem";
+            mantenimientoToolStripMenuItem.Size = new Size(101, 20);
+            mantenimientoToolStripMenuItem.Text = "Mantenimiento";
+            // 
+            // usuariosToolStripMenuItem
+            // 
+            usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
+            usuariosToolStripMenuItem.Size = new Size(180, 22);
+            usuariosToolStripMenuItem.Text = "Usuarios";
+            // 
+            // contratistasToolStripMenuItem
+            // 
+            contratistasToolStripMenuItem.Name = "contratistasToolStripMenuItem";
+            contratistasToolStripMenuItem.Size = new Size(180, 22);
+            contratistasToolStripMenuItem.Text = "Contratistas";
+            // 
+            // tiposDeProgramaToolStripMenuItem
+            // 
+            tiposDeProgramaToolStripMenuItem.Name = "tiposDeProgramaToolStripMenuItem";
+            tiposDeProgramaToolStripMenuItem.Size = new Size(180, 22);
+            tiposDeProgramaToolStripMenuItem.Text = "Tipos de Programa";
+            // 
+            // planesAnualesToolStripMenuItem
+            // 
+            planesAnualesToolStripMenuItem.Name = "planesAnualesToolStripMenuItem";
+            planesAnualesToolStripMenuItem.Size = new Size(180, 22);
+            planesAnualesToolStripMenuItem.Text = "Planes Anuales";
+            // 
+            // gestiónDeProgramasYProyectosToolStripMenuItem
+            // 
+            gestiónDeProgramasYProyectosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { programasToolStripMenuItem, proyectosToolStripMenuItem, avancesDelProyectoToolStripMenuItem });
+            gestiónDeProgramasYProyectosToolStripMenuItem.Name = "gestiónDeProgramasYProyectosToolStripMenuItem";
+            gestiónDeProgramasYProyectosToolStripMenuItem.Size = new Size(199, 20);
+            gestiónDeProgramasYProyectosToolStripMenuItem.Text = "Gestión de Programas y Proyectos";
+            // 
+            // programasToolStripMenuItem
+            // 
+            programasToolStripMenuItem.Name = "programasToolStripMenuItem";
+            programasToolStripMenuItem.Size = new Size(187, 22);
+            programasToolStripMenuItem.Text = "Programas";
+            // 
+            // proyectosToolStripMenuItem
+            // 
+            proyectosToolStripMenuItem.Name = "proyectosToolStripMenuItem";
+            proyectosToolStripMenuItem.Size = new Size(187, 22);
+            proyectosToolStripMenuItem.Text = "Proyectos";
+            // 
+            // avancesDelProyectoToolStripMenuItem
+            // 
+            avancesDelProyectoToolStripMenuItem.Name = "avancesDelProyectoToolStripMenuItem";
+            avancesDelProyectoToolStripMenuItem.Size = new Size(187, 22);
+            avancesDelProyectoToolStripMenuItem.Text = "Avances del Proyecto";
+            // 
+            // gestiónFinancieraToolStripMenuItem
+            // 
+            gestiónFinancieraToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { órdenesDePagoToolStripMenuItem });
+            gestiónFinancieraToolStripMenuItem.Name = "gestiónFinancieraToolStripMenuItem";
+            gestiónFinancieraToolStripMenuItem.Size = new Size(116, 20);
+            gestiónFinancieraToolStripMenuItem.Text = "Gestión Financiera";
+            // 
+            // órdenesDePagoToolStripMenuItem
+            // 
+            órdenesDePagoToolStripMenuItem.Name = "órdenesDePagoToolStripMenuItem";
+            órdenesDePagoToolStripMenuItem.Size = new Size(180, 22);
+            órdenesDePagoToolStripMenuItem.Text = "Órdenes de Pago";
+            // 
+            // consultasReportesToolStripMenuItem
+            // 
+            consultasReportesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { reportesDeProyectosToolStripMenuItem, avancesDeProyectosToolStripMenuItem, resumenDeÓrdenesDePagoToolStripMenuItem, consultasPorContratistasFechasEstadoToolStripMenuItem });
+            consultasReportesToolStripMenuItem.Name = "consultasReportesToolStripMenuItem";
+            consultasReportesToolStripMenuItem.Size = new Size(128, 20);
+            consultasReportesToolStripMenuItem.Text = "Consultas / Reportes";
+            // 
+            // reportesDeProyectosToolStripMenuItem
+            // 
+            reportesDeProyectosToolStripMenuItem.Name = "reportesDeProyectosToolStripMenuItem";
+            reportesDeProyectosToolStripMenuItem.Size = new Size(296, 22);
+            reportesDeProyectosToolStripMenuItem.Text = "Reportes de Proyectos";
+            // 
+            // avancesDeProyectosToolStripMenuItem
+            // 
+            avancesDeProyectosToolStripMenuItem.Name = "avancesDeProyectosToolStripMenuItem";
+            avancesDeProyectosToolStripMenuItem.Size = new Size(296, 22);
+            avancesDeProyectosToolStripMenuItem.Text = "Avances de Proyectos";
+            // 
+            // resumenDeÓrdenesDePagoToolStripMenuItem
+            // 
+            resumenDeÓrdenesDePagoToolStripMenuItem.Name = "resumenDeÓrdenesDePagoToolStripMenuItem";
+            resumenDeÓrdenesDePagoToolStripMenuItem.Size = new Size(296, 22);
+            resumenDeÓrdenesDePagoToolStripMenuItem.Text = "Resumen de Órdenes de Pago";
+            // 
+            // consultasPorContratistasFechasEstadoToolStripMenuItem
+            // 
+            consultasPorContratistasFechasEstadoToolStripMenuItem.Name = "consultasPorContratistasFechasEstadoToolStripMenuItem";
+            consultasPorContratistasFechasEstadoToolStripMenuItem.Size = new Size(296, 22);
+            consultasPorContratistasFechasEstadoToolStripMenuItem.Text = "Consultas por Contratistas, Fechas, Estado";
+            // 
+            // verToolStripMenuItem
+            // 
+            verToolStripMenuItem.Name = "verToolStripMenuItem";
+            verToolStripMenuItem.Size = new Size(35, 20);
+            verToolStripMenuItem.Text = "Ver";
+            // 
+            // ventanasToolStripMenuItem
+            // 
+            ventanasToolStripMenuItem.Name = "ventanasToolStripMenuItem";
+            ventanasToolStripMenuItem.Size = new Size(66, 20);
+            ventanasToolStripMenuItem.Text = "Ventanas";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            ayudaToolStripMenuItem.Size = new Size(53, 20);
+            ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1 });
+            toolStrip1.Location = new Point(0, 24);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(1035, 25);
+            toolStrip1.TabIndex = 1;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(23, 22);
+            toolStripButton1.Text = "toolStripButton1";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 505);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1035, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(118, 17);
+            toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Principal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1035, 527);
+            Controls.Add(statusStrip1);
+            Controls.Add(toolStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Principal";
+            Text = "Principal";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem covialToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem cerrarSesiónToolStripMenuItem;
+        private ToolStripMenuItem mantenimientoToolStripMenuItem;
+        private ToolStripMenuItem usuariosToolStripMenuItem;
+        private ToolStripMenuItem contratistasToolStripMenuItem;
+        private ToolStripMenuItem tiposDeProgramaToolStripMenuItem;
+        private ToolStripMenuItem planesAnualesToolStripMenuItem;
+        private ToolStripMenuItem gestiónDeProgramasYProyectosToolStripMenuItem;
+        private ToolStripMenuItem programasToolStripMenuItem;
+        private ToolStripMenuItem proyectosToolStripMenuItem;
+        private ToolStripMenuItem avancesDelProyectoToolStripMenuItem;
+        private ToolStripMenuItem gestiónFinancieraToolStripMenuItem;
+        private ToolStripMenuItem órdenesDePagoToolStripMenuItem;
+        private ToolStripMenuItem consultasReportesToolStripMenuItem;
+        private ToolStripMenuItem reportesDeProyectosToolStripMenuItem;
+        private ToolStripMenuItem avancesDeProyectosToolStripMenuItem;
+        private ToolStripMenuItem resumenDeÓrdenesDePagoToolStripMenuItem;
+        private ToolStripMenuItem consultasPorContratistasFechasEstadoToolStripMenuItem;
+        private ToolStripMenuItem verToolStripMenuItem;
+        private ToolStripMenuItem ventanasToolStripMenuItem;
+        private ToolStripMenuItem ayudaToolStripMenuItem;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
---
+++ Domain/PlanAnualModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.MySQL;
+using System.Data;
+
+namespace Domain
+{
+    public class PlanAnualModel
+    {
+        //Metodo Insertar que llama al metodo Insertar de la clase PlanAnualDao de la capa de DataAccess
+        public static string Insertar(int año,DateTime fecha_creacion, string nombre_plan, string descripcion, int creado_por)
+        {
+            PlanAnualDao obj = new PlanAnualDao();
+            obj.Año = año;
+            obj.Nombre_plan = nombre_plan;
+            obj.Descripcion = descripcion;
+            obj.Fecha_creacion = fecha_creacion;
+            obj.Creado_por = creado_por;
+            return obj.Insertar(obj);
+        }
+
+        //Metodo Editar que llama al metodo Editar de la clase PlanAnualDao de la capa de DataAccess
+        public static string Editar(int plan_anual_id, int año, string nombre_plan, string descripcion, DateTime fecha_creacion, int creado_por)
+        {
+            PlanAnualDao obj = new PlanAnualDao();
+            obj.Plan_anual_id = plan_anual_id;
+            obj.Año = año;
+            obj.Nombre_plan = nombre_plan;
+            obj.Descripcion = descripcion;
+            obj.Fecha_creacion = fecha_creacion;
+            obj.Creado_por = creado_por;
+            return obj.Editar(obj);
+        }
+
+        //Metodo Eliminar que llama al metodo Eliminar de la clase PlanAnualDao de la capa de DataAccess
+        public static string Eliminar(int plan_anual_id)
+        {
+            PlanAnualDao obj = new PlanAnualDao();
+            obj.Plan_anual_id = plan_anual_id;
+            return obj.Eliminar(obj);
+        }
+
+        //Metodo Mostrar que llama al metodo Mostrar de la clase PlanAnualDao de la capa de DataAccess
+        public static DataTable Mostrar()
+        {
+            return new PlanAnualDao().Mostrar();
+        }
+
+        //Metodo BuscarNombre que llama al metodo BuscarNombre de la clase PlanAnualDao de la capa de DataAccess
+        public static DataTable BuscarNombre(string textobuscar)
+        {
+            PlanAnualDao obj = new PlanAnualDao();
+            obj.Textobuscar = textobuscar;
+            return obj.Buscar(obj);
+        }
+
+        //Metodo MostrarPlanAnualPorAño que llama al metodo MostrarPlanAnualPorAño de la clase PlanAnualDao de la capa de DataAccess
+        public static DataTable MostrarPlanAnualPorAño(int año)
+        {
+            PlanAnualDao obj = new PlanAnualDao();
+            obj.Año = año;
+            return obj.MostrarPlanAnualPorAño(obj);
+        }
+    }
+}
